9566d5f1ee5699e504f07f009aa16d475ed075ec
muhammed adnaj <adnajnaju01@gmail.com>
2024-11-21 15:03:26 +0530

    level2222

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import adnaj from '../assets/adnaj.jpg';
 import { Link } from 'react-scroll';
+import Resume from '../assets/muhammed_Adnaj.pdf'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -137,7 +138,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300' target="_blank"
-              href='/muhammed_Adnaj.pdf' download={true}
+              href={Resume} download={true}
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
