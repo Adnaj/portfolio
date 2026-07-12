a2d8c51c49f21beaec126f9a988b9e938dde9fa6
SuhailDev28 <suhailiqbal28@gmail.com>
2026-07-12 23:03:55 +0300

    Update portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -137,7 +137,7 @@ const Navbar = () => {
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
-              className='flex justify-between items-center w-full text-gray-300' target="_blank"
+              className='flex justify-between items-center w-full text-gray-300' target="_blank" rel="noopener noreferrer"
               href={Resume}
             >
               Resume <BsFillPersonLinesFill size={30} />
